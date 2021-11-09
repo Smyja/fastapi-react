f979c9b0f92ddce8eca46d1b48eef5096f6119ed
Smyja <akposlive59@gmail.com>
2021-11-09 06:29:25 +0100

    adjustv2

---
+++ backend/tests/json.py
@@ -0,0 +1,11 @@
+notice_sample_data = {
+    "_id": "61898407660aa90fb295e2c1",
+    "author_img_url": "http://test.u",
+    "author_name": "string",
+    "author_username": "string",
+    "created": "2021-11-08 13:35:58.893000+00:00",
+    "media": [],
+    "message": "string",
+    "title": "string",
+    "views": "0",
+}
--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -2,43 +2,20 @@ from fastapi.testclient import TestClient
 
 from main import app
 from storage.db import db
+from .json import notice_sample_data
 
 client = TestClient(app)
 
 
 def test_read():
-    notice_sample_data = {
-        "_id": "61898407660aa90fb295e2c1",
-        "author_img_url": "http://test.u",
-        "author_name": "string",
-        "author_username": "string",
-        "created": "2021-11-08 13:35:58.893000+00:00",
-        "media": [],
-        "message": "string",
-        "title": "string",
-        "views": "0",
-    }
     response = db.read("noticeboard", org_id="61767acbbc003777d000a92a")
-
     notices = response["data"]
     assert response["status"] == 200
     assert notices[0] == notice_sample_data
 
 
 def test_view_notices():
-    notice_sample_data = {
-        "_id": "61898407660aa90fb295e2c1",
-        "author_img_url": "http://test.u",
-        "author_name": "string",
-        "author_username": "string",
-        "created": "2021-11-08 13:35:58.893000+00:00",
-        "media": [],
-        "message": "string",
-        "title": "string",
-        "views": "0",
-    }
     response = client.get("/api/v1/organisation/61767acbbc003777d000a92a/notices")
-
     response_data = response.json()
     notices = response_data["data"]
     reversed_list = notices[::-1]
