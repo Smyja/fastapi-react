443ba2fc181a865a8063ebfe4e8b54ba50d69a09
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-11-25 22:15:39 +0100

    add tests for notice

--- backend/tests/test_notice.py
+++ backend/tests/test_notice.py
@@ -1,88 +1,85 @@
-import unittest
 from fastapi.testclient import TestClient
-from unittest.mock import patch
+from unittest import mock
 from main import app
 
-class TestNotice(unittest.TestCase):
 
-    client = TestClient(app)
+client = TestClient(app)
 
-    @patch("main.db.read")
-    def test_view_notice(self, mock_read):
-        response = self.client.get("/api/v1/organisation/619d0966d56a9291379303f0/notices").json()
+@mock.patch("main.db.read")
+def test_view_notice(mock_read):
+    response = client.get("/api/v1/organisation/619d0966d56a9291379303f0/notices").json()
 
-        mock_read.return_value = {
-            "_id": "61898407660aa90fb295e2c1",
-            "author_img_url": "http://test.u",
-            "author_name": "string",
-            "author_username": "string",
-            "created": "2021-11-08 13:35:58.893000+00:00",
-            "media": [],
-            "message": "string",
-            "title": "string",
-            "views": "0",
-        }
+    mock_read.return_value = {
+        "_id": "61898407660aa90fb295e2c1",
+        "author_img_url": "http://test.u",
+        "author_name": "string",
+        "author_username": "string",
+        "created": "2021-11-08 13:35:58.893000+00:00",
+        "media": [],
+        "message": "string",
+        "title": "string",
+        "views": "0",
+    }
 
-        self.assertEqual(response['message'], "retrieved successfully")
+    assert response['message'] == "retrieved successfully"
 
-    @patch('main.db.save')
-    def test_create_notice(self, mock_save):
-        payload = {
-            "author_img_url": "http://test.u",
-            "author_name": "string",
-            "author_username": "string",
-            "media": [],
-            "message": "string",
-            "title": "string",
-            "views": "0",
-        }
+@mock.patch('main.db.save')
+def test_create_notice(mock_save):
+    payload = {
+        "author_img_url": "http://test.u",
+        "author_name": "string",
+        "author_username": "string",
+        "media": [],
+        "message": "string",
+        "title": "string",
+        "views": "0",
+    }
+
+    mock_save.return_value = {
+        "_id": "61898407660aa90fb295e2c1",
+        "author_img_url": "http://test.u",
+        "author_name": "string",
+        "author_username": "string",
+        "created": "2021-11-08 13:35:58.893000+00:00",
+        "media": [],
+        "message": "string",
+        "title": "string",
+        "views": "0",
+    }
+
+    response = client.post("/api/v1/organisation/619d0966d56a9291379303f0/create", json=payload).json()
     
-        mock_save.return_value = {
-            "_id": "61898407660aa90fb295e2c1",
-            "author_img_url": "http://test.u",
-            "author_name": "string",
-            "author_username": "string",
-            "created": "2021-11-08 13:35:58.893000+00:00",
-            "media": [],
-            "message": "string",
-            "title": "string",
-            "views": "0",
-        }
+    assert response["data"]["author_img_url"] == "http://test.u"
+    assert response["message"] == "successfully created"
+    assert (response["success"]) == True
 
-        response = self.client.post("/api/v1/organisation/619d0966d56a9291379303f0/create", json=payload).json()
-        
-        self.assertEqual(response["data"]["author_img_url"], "http://test.u")
-        self.assertEqual(response["message"], "successfully created")
-        self.assertTrue(response["success"])
+@mock.patch("main.db.delete")
+def test_delete_notice(mock_delete):
+    response = client.delete("/api/v1/organisation/619d0966d56a9291379303f0/notices/61898407660aa90fb295e2c1/delete").json()
 
-    @patch("main.db.delete")
-    def test_delete_notice(self, mock_delete):
-        response = self.client.delete("/api/v1/organisation/619d0966d56a9291379303f0/notices/61898407660aa90fb295e2c1/delete").json()
+    mock_delete.return_value = {}
 
-        mock_delete.return_value = {}
+    assert response['message'] == "Delete Operation Successful"
 
-        self.assertEqual(len(response["data"]), 0)
-        self.assertEqual(response['message'], "Delete Operation Successful")
+@mock.patch('main.db.update')
+def test_update_notice(mock_update):
+    payload = {
+        "title": "test",
+    }
 
-    @patch('main.db.update')
-    def test_update_notice(self, mock_update):
-        payload = {
-            "title": "test",
-        }
+    mock_update.return_value = {
+        "_id": "61898407660aa90fb295e2c1",
+        "author_img_url": "http://test.u",
+        "author_name": "string",
+        "author_username": "string",
+        "created": "2021-11-08 13:35:58.893000+00:00",
+        "media": [],
+        "message": "string",
+        "title": "test",
+        "views": "0",
+    }
 
-        mock_update.return_value = {
-            "_id": "61898407660aa90fb295e2c1",
-            "author_img_url": "http://test.u",
-            "author_name": "string",
-            "author_username": "string",
-            "created": "2021-11-08 13:35:58.893000+00:00",
-            "media": [],
-            "message": "string",
-            "title": "test",
-            "views": "0",
-        }
-
-        response = self.client.patch("/api/v1/organisation/619d0966d56a9291379303f0/notices/61898407660aa90fb295e2c1/edit", json=payload).json()
-        
-        self.assertEqual(response['data']['title'], 'test')
-        self.assertEqual(response['message'],"Notice has been successfully updated")
+    response = client.patch("/api/v1/organisation/619d0966d56a9291379303f0/notices/61898407660aa90fb295e2c1/edit", json=payload).json()
+    
+    assert response['data']['title'] == 'test'
+    assert response['message'] == "Notice has been successfully updated"
