88d547efe38dbb8a895ce349de9c8a183f833807
Smyja <akposlive59@gmail.com>
2022-06-25 16:20:11 +0100

    Update test_notice.py

--- backend/tests/test_notice.py
+++ backend/tests/test_notice.py
@@ -15,20 +15,6 @@ def test_view_notice(mock_read):
     print(response)
     assert response.status_code == 200
 
-    # mock_read.return_value = {
-    #     "_id": "61898407660aa90fb295e2c1",
-    #     "author_img_url": "http://test.u",
-    #     "author_name": "string",
-    #     "author_username": "string",
-    #     "created": "2021-11-08 13:35:58.893000+00:00",
-    #     "media": [],
-    #     "message": "string",
-    #     "title": "string",
-    #     "views": "0",
-    # }
-
-    # assert response["message"] == "retrieved successfully"
-
 @mock.patch('main.db.save')
 def test_create_notice(mock_save):
     payload = {
@@ -40,18 +26,6 @@ def test_create_notice(mock_save):
         "title": "string",
         "views": "0",
     }
-
-    # mock_save.return_value = {
-    #     "_id": "61898407660aa90fb295e2c1",
-    #     "author_img_url": "http://test.u",
-    #     "author_name": "string",
-    #     "author_username": "string",
-    #     "created": "2021-11-08 13:35:58.893000+00:00",
-    #     "media": [],
-    #     "message": "string",
-    #     "title": "string",
-    #     "views": "0",
-    # }
 
     response = client.post("/api/v1/organisation/619d0966d56a9291379303f0/create", json=payload).json()
     print(response)
